0935fbafa7e972e42945f55651b05fd36c763f03
MarKraken <mvisapascual@gmail.com>
2019-12-20 20:10:58 +0100

    Add settings, upload, data_analytics, variable_selector, line_chart html files for base application structure.

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('settings', settings, name='settings'),
+    path('upload', upload, name='upload'),
+    path('data_analytics', data_analytics, name='data analytics'),
+    path('variable_selector', variable_selector, name='variable selector'),
+    path('line_chart', line_chart, name='line chart'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
 
 def home(request):
         return render(request, 'base.html')
+
+
+def settings(request):
+        return render(request, 'settings.html')
+
+
+def upload(request):
+        return render(request, 'upload.html')
+
+
+def data_analytics(request):
+        return render(request, 'data_analytics.html')
+
+
+def variable_selector(request):
+        return render(request, 'variable_selector.html')
+
+
+def line_chart(request):
+        return render(request, 'line_chart.html')
