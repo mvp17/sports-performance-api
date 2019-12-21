02e3398218c231462579a25cd5dd500e715e7ac3
MarKraken <mvisapascual@gmail.com>
2019-12-21 21:49:45 +0100

    Change views to class-based views.

--- app/urls.py
+++ app/urls.py
@@ -5,9 +5,8 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('settings', configuration, name='settings'),
-    # path('upload', upload, name='upload'),
-    path('files', file_list, name='file_list'),
-    path('files/upload', upload_file, name='upload_file'),
+    path('files', FileList.as_view(), name='file_list'),
+    path('files/upload', UploadFile.as_view(), name='upload_file'),
     path('data_analytics', data_analytics, name='data analytics'),
     path('variable_selector', variable_selector, name='variable selector'),
     path('line_chart', line_chart, name='line chart'),
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-# from django.core.files.storage import FileSystemStorage
 from .forms import FileForm
+from django.views.generic import *
+from django.urls import reverse_lazy
 from .models import PerformanceData
 
 
@@ -11,36 +12,18 @@ def home(request):
 def configuration(request):
     return render(request, 'settings.html')
 
-"""
-def upload(request):
-    context = {}
-    if request.method == 'POST':
-        uploaded_file = request.FILES['document']
-        fs = FileSystemStorage()
-        name = fs.save(uploaded_file.name, uploaded_file)
-        context['url'] = fs.url(name)
-    return render(request, 'upload.html', context)
-"""
+
+class FileList(ListView):
+    model = PerformanceData
+    template_name = 'uploading/file_list.html'
+    context_object_name = 'files'
 
 
-def file_list(request):
-    files = PerformanceData.objects.all()
-    return render(request, 'file_list.html', {
-        'files': files
-    })
-
-
-def upload_file(request):
-    if request.method == 'POST':
-        form = FileForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('file_list')
-    else:
-        form = FileForm()
-    return render(request, 'upload_file.html', {
-        'form': form
-    })
+class UploadFile(CreateView):
+    model = PerformanceData
+    form_class = FileForm
+    success_url = reverse_lazy('file_list')
+    template_name = 'uploading/upload_file.html'
 
 
 def data_analytics(request):
