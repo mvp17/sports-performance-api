c5492651979937c910e8207cf3dbf4c83ba8a735
MarKraken <mvisapascual@gmail.com>
2020-03-13 15:59:18 +0100

    test heroku static files 2

--- TFG/settings.py
+++ TFG/settings.py
@@ -126,8 +126,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = (os.path.join(BASE_DIR, 'staticfiles'))
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
-                    )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/app/'
